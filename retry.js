59174fb7c3d5ed72e52f805517397952f51e1b1d
seon <seon.piao@gmail.com>
2014-01-03 19:25:30 +0800

    init!

---
+++ retry.js
@@ -0,0 +1,76 @@
+(function(global){
+    var Retry = function(options){
+        options = options || {};
+        this._timeout = options.timeout || 30000;
+        this._max = options.max || Infinity;
+        this._interval = options.interval || 0;
+        this._cbs = {
+            done:[],
+            fail:[],
+            timeout:[]
+        };
+    };
+    Retry.prototype = {
+        start:function(fn){
+            var tried = 0;
+            var _this = this;
+            var timer,stop = false;
+            timer = setTimeout(function(){
+                _this._fire('timeout');
+                stop = true;
+            },_this._timeout);
+            (function tryfn(){
+                if(!stop){
+                    tried++;
+                    if(tried > _this._max){
+                        _this._fire('fail');
+                        clearTimeout(timer);
+                    }
+                    else{
+                        fn(function(done){
+                            if(done === true){
+                                _this._fire('done');
+                                clearTimeout(timer);
+                            }
+                            else{
+                                if(_this._interval){
+                                    setTimeout(tryfn,_this._interval);
+                                }
+                                else{
+                                    tryfn();
+                                }
+                            }
+                        });
+                    }    
+                }
+            })();
+        },
+        on:function(type,cb){
+            var cbs = this._cbs[type];
+            cbs.push(cb);
+        },
+        un:function(type,cb){
+            var cbs = this._cbs[type];
+            if(cb){
+                var i = cbs.indexOf(cb);
+                if(i !== -1){
+                    cbs.splice(i,1);
+                }
+            }
+            else{
+                cbs.splice(0);
+            }
+        },
+        _fire:function(type){
+            var cbs = this._cbs[type];
+            if(cbs){
+                for(var i = 0; i < cbs.length; i++){
+                    cbs[i]();
+                }
+            }
+        }
+    };
+    if(typeof module !== 'undefined'){
+        module.exports = Retry;
+    }
+})(this);
---
+++ tests/test.js
@@ -0,0 +1,130 @@
+var Retry = require('../retry');
+
+var assert = require("assert");
+
+describe('Retry', function() {
+    describe('try', function() {
+        it('try until done', function(done) {
+            var retry = new Retry();
+            var n = 0;
+            retry.on('done',function(){
+                assert.equal(10,n);
+                done();
+            });
+            retry.start(function(cb){
+                n++;
+                if(n === 10){
+                    cb(true);
+                }
+                else{
+                    cb();
+                }
+            });
+        });
+    });
+    describe('max',function(){
+        it('try until max reached.', function(done) {
+            var retry = new Retry({
+                max:10
+            });
+            var n = 0;
+            retry.on('fail',function(){
+                assert.equal(10,n);
+                done();
+            });
+            retry.start(function(cb){
+                n++;
+                cb();
+            });
+        });
+    });
+    describe('timeout',function(){
+        it('try until timeout.', function(done) {
+            this.timeout(2000);
+            var retry = new Retry({
+                timeout:1000
+            });
+            retry.on('timeout',function(){
+                done();
+            });
+            retry.start(function(cb){
+                setTimeout(cb,2000);
+            });
+        });
+        it('stop when timeout.', function(done) {
+            this.timeout(10000);
+            var retry = new Retry({
+                timeout:1000,
+                interval:210
+            });
+            var n = 0;
+            retry.on('timeout',function(){
+                setTimeout(function(){
+                    assert.equal(5,n);
+                    done();
+                },500);
+            });
+            retry.start(function(cb){
+                n++;
+                cb();
+            });
+        });
+    });
+    describe('events', function() {
+        it('add multi callbacks', function(done) {
+            var retry = new Retry();
+            var n = 0;
+            retry.on('done',function(){
+                n++;
+            });
+            retry.on('done',function(){
+                n++;
+            });
+            retry.on('done',function(){
+                assert.equal(2,n);
+                done();
+            });
+            retry.start(function(cb){
+                cb(true);
+            });
+        });
+        it('remove callback', function(done) {
+            var retry = new Retry();
+            var n = 0;
+            retry.on('done',function(){
+                n++;
+            });
+            var toRemove = function(){
+                n++;
+            };
+            retry.on('done',toRemove);
+            retry.on('done',function(){
+                assert.equal(1,n);
+                done();
+            });
+            retry.un('done',toRemove);
+            retry.start(function(cb){
+                cb(true);
+            });
+        });
+        it('remove all callbacks', function(done) {
+            var retry = new Retry();
+            var n = 0;
+            retry.on('done',function(){
+                n++;
+            });
+            var toRemove = function(){
+                n++;
+            };
+            retry.on('done',toRemove);
+            retry.un('done');
+            retry.on('done',function(){
+                assert.equal(0,n);
+                done();
+            });
+            retry.start(function(cb){
+                cb(true);
+            });
+        });
+    });
+});
